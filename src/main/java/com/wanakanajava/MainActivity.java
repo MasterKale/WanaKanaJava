c98955c25dfa279d4c43f4e464a34b09e4c7224b
Matthew Miller <matthew@knwd.us>
2013-10-23 17:05:53 -0700

    Initial Code Commit

---
+++ src/main/java/com/wanakanajava/MainActivity.java
@@ -0,0 +1,50 @@
+package com.wanakanajava;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null)
+        {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment
+    {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+	        EditText et = (EditText) rootView.findViewById(R.id.editText);
+
+	        WanaKanaJava wkj = new WanaKanaJava(et, false);
+	        wkj.bind();
+
+            return rootView;
+        }
+    }
+
+}
